7846f902911eda73e34b8dc33db013bc8afc7285
chris-chak <chris.chak@ezypay.com>
2021-03-04 14:16:04 +1100

    initial commit

---
+++ src/main/java/com/chris/jdk15test/Jdk15TestApplication.java
@@ -0,0 +1,13 @@
+package com.chris.jdk15test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jdk15TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jdk15TestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chris/jdk15test/Jdk15TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chris.jdk15test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jdk15TestApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
